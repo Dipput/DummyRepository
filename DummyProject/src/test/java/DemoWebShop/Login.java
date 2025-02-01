43416d703d65c10721b0562fcfe9407dc2b82010
Pawan <Pawan@LAPTOP-L8DJVMDE>
2025-02-01 13:03:29 +0530

    DummyProjectDemoWebShop

---
+++ DummyProject/src/test/java/DemoWebShop/Login.java
@@ -0,0 +1,22 @@
+package DemoWebShop;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Login
+{
+	@Test
+	public void loginLink()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://demowebshop.tricentis.com/");
+		Reporter.log("Official site is launched",true);
+	}
+
+}
