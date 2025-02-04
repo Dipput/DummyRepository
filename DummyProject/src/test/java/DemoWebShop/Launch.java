a2ae4e83668df650fe9281193e85e68c80f9eea0
vipen <vipen@Hi-Speed-S340>
2025-02-04 11:09:08 +0530

    Product launched

---
+++ DummyProject/src/test/java/DemoWebShop/Launch.java
@@ -0,0 +1,13 @@
+package DemoWebShop;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Launch {
+
+	@Test
+	public void launch()
+	{
+		Reporter.log("page launched",true);
+	}
+}
