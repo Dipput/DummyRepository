97655d2780462a0a535426386b74650b0159e440
Pawan <Pawan@LAPTOP-L8DJVMDE>
2025-02-05 10:25:50 +0530

    Synching

---
+++ DummyProject/src/test/java/DemoWebShop/SynchFork.java
@@ -0,0 +1,15 @@
+package DemoWebShop;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SynchFork 
+{
+	@Test
+	public void synch()
+	{
+		Reporter.log("Synched Fork successfully",true);
+	}
+	
+
+}
